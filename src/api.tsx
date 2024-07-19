2df091c2ff6836996cab9d1ddb04fbb1b74e81dd
Sarika Mangalpally <39065053+SarikaMangalpally@users.noreply.github.com>
2024-07-19 18:22:21 -0500

    api url updated

--- src/api.tsx
+++ src/api.tsx
@@ -1,24 +1,24 @@
 import axios from 'axios';
 import {RecipeType} from './types/recipe';
 
-const API_URL = 'http://ec2-54-215-226-8.us-west-1.compute.amazonaws.com:8000/api'
+const API_URL = 'http://ec2-13-57-8-175.us-west-1.compute.amazonaws.com:8000/api'
 // const API_URL = 'http://127.0.0.1:8000/api';
 
 export const getRecipes = () => {
-  return axios.get(`${API_URL}/recipes/`); // Modified URL
+  return axios.get(`${API_URL}/recipes/`);
 };
 export const getRecipeById = (id: number) => {
-  return axios.get(`${API_URL}/recipes/${id}/`); // Modified URL
+  return axios.get(`${API_URL}/recipes/${id}/`);
 };
 
 export const createRecipe = (recipe: RecipeType) => {
-  return axios.post(`${API_URL}/recipes/`, recipe); // Modified URL
+  return axios.post(`${API_URL}/recipes/`, recipe);
 };
 
 export const updateRecipe = (id: number, recipe: RecipeType) => {
-  return axios.put(`${API_URL}/recipes/${id}/`, recipe); // Modified URL
+  return axios.put(`${API_URL}/recipes/${id}/`, recipe);
 };
 
 export const deleteRecipe = (id: number) => {
-  return axios.delete(`${API_URL}/recipes/${id}/`); // Modified URL
+  return axios.delete(`${API_URL}/recipes/${id}/`);
 };
